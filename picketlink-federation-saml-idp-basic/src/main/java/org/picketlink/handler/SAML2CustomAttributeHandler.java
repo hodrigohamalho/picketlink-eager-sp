3a5d94641dbdbc1a9ff062ecd222f775f85008e7
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2014-08-25 19:08:38 -0300

    Logic to get IDP ID

--- picketlink-federation-saml-idp-basic/src/main/java/org/picketlink/handler/SAML2CustomAttributeHandler.java
+++ picketlink-federation-saml-idp-basic/src/main/java/org/picketlink/handler/SAML2CustomAttributeHandler.java
@@ -1,19 +1,29 @@
 package org.picketlink.handler;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.security.jacc.PolicyContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.picketlink.identity.federation.core.interfaces.AttributeManager;
 
 public class SAML2CustomAttributeHandler implements AttributeManager{
 
 	@Override
-	public Map<String, Object> getAttributes(Principal userPrincipal,
-			List<String> attributeKeys) {
-		System.out.println("OI");
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Object> getAttributes(Principal userPrincipal, List<String> attributeKeys) {
+    	Map<String, Object> m = new HashMap<String, Object>();
+    	
+	    try{
+	    	HttpServletRequest request = (HttpServletRequest) PolicyContext.getContext("javax.servlet.http.HttpServletRequest");
+			m.put("sessionID", request.getSession().getId());
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+		
+		return m;
 	}
 
 }
--- picketlink-federation-saml-sp-central/src/main/java/org/picketlink/utils/PicketlinkUtil.java
+++ picketlink-federation-saml-sp-central/src/main/java/org/picketlink/utils/PicketlinkUtil.java
@@ -1,7 +1,8 @@
 package org.picketlink.utils;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,7 +17,6 @@ import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 import org.jsoup.Jsoup;
@@ -25,122 +25,135 @@ import org.jsoup.nodes.Element;
 
 public class PicketlinkUtil implements Filter {
 
-    private Header[] headers;
+	private Header[] headers;
 
 	@Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 		HttpSession session = ((HttpServletRequest) request).getSession();
-    	
-    	// Request pro destino (SP)
-    	String html = Request.Get("http://localhost:8080/sales-post/").execute().handleResponse(new ResponseHandler<String>() {
-            @Override            
-            public String handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
-                headers = httpResponse.getAllHeaders();
+		String IDPSessionID = getIDPSessionID(session);
 
-                return IOUtils.getStringFromInputStream(httpResponse.getEntity().getContent());
-            }
-        });
+		// Request pro destino (SP)
+		String html = Request.Get("http://localhost:8080/sales-post/").addHeader("cookie", "JSESSIONID="+IDPSessionID).execute().handleResponse(new ResponseHandler<String>() {
+			@Override            
+			public String handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
+				headers = httpResponse.getAllHeaders();
 
-        String SAMLRequest = getAttributeValueFromHtml(html, "SAMLRequest");
-        String actionURL = getAttributeActionFromHtml(html);
-        
-
-        // Request pro IDP com o SAMLRequest
-        Request req = Request.Post(actionURL).bodyForm(Form.form().add("SAMLRequest", SAMLRequest).build());
-        req.addHeader("content-type","application/x-www-form-urlencoded; charset=UTF-8");
-        req.addHeader("origin", "http://localhost:8080");
-        req.addHeader("accept", "*/*");
-        req.addHeader("host","localhost:8080");
-        req.addHeader("accept-encoding","gzip,deflate,sdch");
-        
-//        for (Header header : headers) {
-//			req.addHeader(header);
-//		}
-        html = req.execute().handleResponse(new ResponseHandler<String>() {
-        	@Override
-        	public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-        		headers = response.getAllHeaders();
-
-                return IOUtils.getStringFromInputStream(response.getEntity().getContent());
-        	}
-        	
+				return IOUtils.getStringFromInputStream(httpResponse.getEntity().getContent());
+			}
 		});
-        
-//    	System.out.println("-------------------------------------");
-//    	System.out.println("LOGANDO!");
-//    	System.out.println("LOGANDO!");
-//    	System.out.println("-------------------------------------");
-    	
-        filterChain.doFilter(request, response);
-    }
+
+		if (html.contains("HTTP Post Binding (Request)")){
+			String SAMLRequest = getAttributeValueFromHtml(html, "SAMLRequest");
+			String idpURL = getAttributeActionFromHtml(html);
+
+			// Request pro IDP com o SAMLRequest
+			Request req = Request.Post(idpURL).bodyForm(Form.form().add("SAMLRequest", SAMLRequest).build());
+			req.addHeader("cookie", "JSESSIONID="+IDPSessionID);
+
+			html = req.execute().handleResponse(new ResponseHandler<String>() {
+				@Override
+				public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+					headers = response.getAllHeaders();
+
+					return IOUtils.getStringFromInputStream(response.getEntity().getContent());
+				}
+
+			});
+
+			String SAMLResponse = getAttributeValueFromHtml(html, "SAMLResponse");
+			String spURL = getAttributeActionFromHtml(html);
+
+			// Request pro SP com o SAMLResponse
+			Request reqSP = Request.Post(spURL).bodyForm(Form.form().add("SAMLResponse", SAMLResponse).build());
+			req.addHeader("cookie", "JSESSIONID="+IDPSessionID);
+
+			html = reqSP.execute().handleResponse(new ResponseHandler<String>() {
+				@Override
+				public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+					headers = response.getAllHeaders();
+
+					return IOUtils.getStringFromInputStream(response.getEntity().getContent());
+				}
+
+			});
+		}
+		
+		System.out.println("HTML: "+html);
+		filterChain.doFilter(request, response);
+	}
 
 
-//    private String httpPost(String url, List <NameValuePair> postParams) throws IOException {
-//        String html;
-//
-//        CloseableHttpClient httpclient = HttpClients.createDefault();
-//        HttpPost httpPost = new HttpPost(url);
-//
-//        httpPost.setEntity(new UrlEncodedFormEntity(postParams));
-//        CloseableHttpResponse response2 = httpclient.execute(httpPost);
-//
-//        try {
-//            HttpEntity entity2 = response2.getEntity();
-//            html = IOUtils.getStringFromInputStream(entity2.getContent());
-//            EntityUtils.consume(entity2);
-//        } finally {
-//            response2.close();
-//        }
-//
-//        return html;
-//    }
-//
-//    private String httpGet(String url) throws IOException {
-//        String html;
-//
-//        CloseableHttpClient httpclient = HttpClients.createDefault();
-//        HttpGet httpGet = new HttpGet(url);
-//        CloseableHttpResponse response1 = httpclient.execute(httpGet);
-//
-//        try {
-//            HttpEntity entity1 = response1.getEntity();
-//            html = IOUtils.getStringFromInputStream(entity1.getContent());
-//            EntityUtils.consume(entity1);
-//        } finally {
-//            response1.close();
-//        }
-//
-//        return html;
-//    }
-//
-    private String getAttributeValueFromHtml(String htmlContent, String attribute){
-        String value = "";
+	@SuppressWarnings("unchecked")
+	private String getIDPSessionID(HttpSession session){
+		Map<String, Object> map = (Map<String, Object>) session.getAttribute("SESSION_ATTRIBUTE_MAP");
+		return (String) ((List<Object>) map.get("sessionID")).get(0);
+	}
+	//    private String httpPost(String url, List <NameValuePair> postParams) throws IOException {
+	//        String html;
+	//
+	//        CloseableHttpClient httpclient = HttpClients.createDefault();
+	//        HttpPost httpPost = new HttpPost(url);
+	//
+	//        httpPost.setEntity(new UrlEncodedFormEntity(postParams));
+	//        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+	//
+	//        try {
+	//            HttpEntity entity2 = response2.getEntity();
+	//            html = IOUtils.getStringFromInputStream(entity2.getContent());
+	//            EntityUtils.consume(entity2);
+	//        } finally {
+	//            response2.close();
+	//        }
+	//
+	//        return html;
+	//    }
+	//
+	//    private String httpGet(String url) throws IOException {
+	//        String html;
+	//
+	//        CloseableHttpClient httpclient = HttpClients.createDefault();
+	//        HttpGet httpGet = new HttpGet(url);
+	//        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+	//
+	//        try {
+	//            HttpEntity entity1 = response1.getEntity();
+	//            html = IOUtils.getStringFromInputStream(entity1.getContent());
+	//            EntityUtils.consume(entity1);
+	//        } finally {
+	//            response1.close();
+	//        }
+	//
+	//        return html;
+	//    }
+	//
+	private String getAttributeValueFromHtml(String htmlContent, String attribute){
+		String value = "";
 
-        Document doc = Jsoup.parse(htmlContent);
-        // Get SAMLRequest value
-        for (Element el : doc.select("input")){
-            if (el.attr("name").equals(attribute)){
-                value = el.attr("value");
-            }
-        }
+		Document doc = Jsoup.parse(htmlContent);
+		// Get SAMLRequest value
+		for (Element el : doc.select("input")){
+			if (el.attr("name").equals(attribute)){
+				value = el.attr("value");
+			}
+		}
 
-        return value;
-    }
+		return value;
+	}
 
-    private String getAttributeActionFromHtml(String htmlContent){
-        Document doc = Jsoup.parse(htmlContent);
-        return doc.select("form").attr("action");
-    }
+	private String getAttributeActionFromHtml(String htmlContent){
+		Document doc = Jsoup.parse(htmlContent);
+		return doc.select("form").attr("action");
+	}
 
-    private String getUrl(ServletRequest request){
-        return request.getScheme() +"://"+ request.getServerName() + ":"+ request.getServerPort() + "/";
-    }
+	private String getUrl(ServletRequest request){
+		return request.getScheme() +"://"+ request.getServerName() + ":"+ request.getServerPort() + "/";
+	}
 
-    @Override
-    public void destroy() {}
+	@Override
+	public void destroy() {}
 }
